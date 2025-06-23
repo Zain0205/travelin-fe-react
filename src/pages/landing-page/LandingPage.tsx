9e378df6d276ed1735576ad38cbdf565806e1c43
Fahmi Zain <fahmizain000@gmail.com>
2025-06-23 21:14:08 +0700

    Create LandingPage.tsx

---
+++ src/pages/landing-page/LandingPage.tsx
@@ -0,0 +1,17 @@
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import TripList from "./TripList";
+
+function LandingPage() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <TripList />
+      <Footer />
+    </>
+  )
+}
+
+export default LandingPage;
