ab5f8f4ae1d9448ebda68345515d3c89a9db6ad7
Fahmi Zain <fahmizain000@gmail.com>
2025-07-01 10:33:37 +0700

    Update LayoutWrapper.tsx

--- src/pages/dashboard/components/LayoutWrapper.tsx
+++ src/pages/dashboard/components/LayoutWrapper.tsx
@@ -1,23 +1,42 @@
 "use client";
 
-import type React from "react";
-
+import { Outlet, useLocation } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 import { SidebarProvider, SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
-import DashboardSidebar from "./DashboardSidebar";
+import { useLayoutEffect } from "react";
 
 interface LayoutWrapperProps {
-  children: React.ReactNode;
+  sidebar: React.ReactNode;
 }
 
-function LayoutWrapper({ children }: LayoutWrapperProps) {
+function LayoutWrapper({ sidebar }: LayoutWrapperProps) {
+  const location = useLocation();
+
+  useLayoutEffect(() => {
+    console.log("Route changed to:", location.pathname);
+  }, [location.pathname]);
+
+
   return (
     <SidebarProvider>
-      <DashboardSidebar />
+      {sidebar}
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
           <SidebarTrigger className="-ml-1" />
         </header>
-        <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">{children}</div>
+        <div className="flex-1 p-4 md:p-8 pt-6">
+          <AnimatePresence mode="wait" onExitComplete={() => console.log("Exit Animation")}>
+            <motion.div
+              key={location.pathname}
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -20 }}
+              transition={{ duration: 0.4, ease: "easeInOut" }  }
+            >
+              <Outlet />
+            </motion.div>
+          </AnimatePresence>
+        </div>
       </SidebarInset>
     </SidebarProvider>
   );
