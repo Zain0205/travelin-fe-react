6b5acbd80f684bdf49a43f2faa5f9a04f476e914
Fahmi Zain <fahmizain000@gmail.com>
2025-07-01 10:34:41 +0700

    Update store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,28 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
+import flightReducer from "./slices/flightSlice";
+import travelPackageReducer from "./slices/travelPackageSlice";
+import hotelReducer from "./slices/hotelSlice"; // Add this import
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    flight: flightReducer,
+    travelPackage: travelPackageReducer,
+    hotel: hotelReducer, // Add this line
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
         ignoredActions: ["persist/PERSIST"],
+        ignoredPaths: [
+          "flight.currentFlight.departureTime", 
+          "flight.currentFlight.arrivalTime",
+          "travelPackage.packages",
+          "travelPackage.currentPackage",
+          "hotel.hotels", // Add hotel paths
+          "hotel.currentHotel",
+        ],
       },
     }),
 });
